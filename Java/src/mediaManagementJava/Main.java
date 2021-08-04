6d436d331022b0e66909a99ea4fcc37143fe6b67
Enrico Guerra <enrico.guerra002@stud.fh-dortmund.de>
2021-08-04 21:46:24 +0200

    REPL in main

--- Java/src/mediaManagementJava/Main.java
+++ Java/src/mediaManagementJava/Main.java
@@ -7,18 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		REPL repl = new REPL();
-//		MediaManagement mediaManagement = new MediaManagement();
-//		while (true) {
-//			System.out.print("Media Management-App>");
-//			Scanner scanner = new Scanner(System.in);
-//			switch (scanner.nextLine()) {
-//				case "show":
-//					System.out.println("All the Media: ...");
-//					break;
-//				default:
-//					break;
-//			}
-//		}
+		repl.Start();
 	}
 
 }
--- Java/src/mediaManagementJava/presentation/REPL.java
+++ Java/src/mediaManagementJava/presentation/REPL.java
@@ -1,5 +1,28 @@
 package mediaManagementJava.presentation;
 
+import java.util.Scanner;
+import mediaManagementJava.logic.MediaManagement;
+
 public class REPL {
+
+	public void Start() {
+		System.out.println("***************************");
+		System.out.println("         THIS IS MY        ");
+		System.out.println("            JAVA           ");
+		System.out.println("   MEDIA MANAGEMENT APP!   ");
+		System.out.println("***************************");
+		while (true) {
+			System.out.print("Media Management-App>");
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+			switch (scanner.nextLine()) {
+			case "show":
+				System.out.println("All the Media: ...");
+				break;
+			default:
+				break;
+			}
+		}
+	}
 
 }
